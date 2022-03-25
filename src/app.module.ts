016a921c47b5e4b218069126947b897c653f0205
duyhp <duyhp2405@gmail.com>
2022-03-25 10:50:09 +0700

    fix deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ declare module 'express' {
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'db',
       port: 3306,
       username: 'root',
       password: '1',
